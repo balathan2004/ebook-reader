3ca2116a43a70d1943d2eae9102eb9cbaa38f97e
massedits <balathan2vijay004@gmail.com>
2023-10-14 16:14:09 +0530

    14Oct4:14

--- components/config.js
+++ components/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaAtESAWssWPTv9dMXKSvWjz-4WfZUbJg",
-  authDomain: "e-book-61c4f.firebaseapp.com",
-  projectId: "e-book-61c4f",
-  storageBucket: "e-book-61c4f.appspot.com",
-  messagingSenderId: "345891582572",
-  appId: "1:345891582572:web:df3fdcc1f3802c37749e49",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
