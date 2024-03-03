a011d86fff3e61db6fcdb3a7f3a6520c2e94f1cf
massedits <balathan2vijay004@gmail.com>
2024-03-03 15:38:53 +0530

    03-03 03:33Pm

--- components/workLoad.js
+++ components/workLoad.js
@@ -1,3 +1,7 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({
+  id: "my-app",
+  eventKey:
+    "PcUJaRxbAPVcSKEyufDb0iVM_riVuIaWiyja8zg2B__asqVYpYI2a20nKs-RDcZLd18wxVcZBCqCOvRQJJcygQ",
+});
