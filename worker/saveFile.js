b994fabe63345baba1cc5ed50ce2bbc7e457059b
massedits <balathan2vijay004@gmail.com>
2024-03-11 20:06:02 +0530

    11-3 7/21

--- worker/saveFile.js
+++ worker/saveFile.js
@@ -25,3 +25,22 @@ async function convertFile(fileUrl, fileName, uid) {
   const jsonFile = fs.readFileSync(fileNameWithPath);
   const pdfUrl = await uploadFile(jsonFile, newFileName, uid, "books");
 }
+
+/** 
+import { inngest } from "@/workLoad";
+import { serve } from "inngest/next";
+import { helloWorld } from "@/worker/saveFile";
+
+
+await inngest.send({
+  name: "mailer",
+  id: "mailer",
+  data: { username: "leo" },
+});
+
+
+export default serve({
+  client: inngest,
+  functions: [helloWorld],
+});
+*/
