e476df4e96bab1eb0026cda1d893d3c6db195d89
massedits <balathan2vijay004@gmail.com>
2024-03-12 21:07:11 +0530

    12-3 9:7

--- components/uploadFileString.js
+++ components/uploadFileString.js
@@ -4,4 +4,5 @@ import { storage } from "./config";
 export default async function uploadFileString(textContent, fileName, uid) {
   const storageRef = ref(storage, `books/${uid}/${fileName}`);
   await uploadString(storageRef, JSON.stringify(textContent)).then(console.log);
+  console.log("success");
 }
