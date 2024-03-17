4cbc58dcff5978b793375012d24211b0790ae323
massedits <balathan2vijay004@gmail.com>
2024-03-17 20:22:36 +0530

    17-3

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const dir = "node_modules/pdfjs-dist/es5/build/pdf.js";
+const dir = "node_modules/pdfjs-dist/legacy/build/pdf.min.mjs";
 const content = fs.readFileSync(dir, { encoding: "utf-8" });
 fs.writeFileSync(
   dir,
