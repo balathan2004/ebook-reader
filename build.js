a49ff1dfa03e3f9b0f8de29a277a2e4b0f4a5921
massedits <balathan2vijay004@gmail.com>
2024-03-17 20:28:11 +0530

    17-3

--- build.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const dir = "node_modules/pdfjs-dist/legacy/build/pdf.min.mjs";
-const content = fs.readFileSync(dir, { encoding: "utf-8" });
-fs.writeFileSync(
-  dir,
-  content.replace('"./pdf.worker.js";', `__dirname + "/pdf.worker.js";`)
-);
