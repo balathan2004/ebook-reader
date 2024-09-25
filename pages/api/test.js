bdd8695922e3db0636d94c4a25f0e5b6ffdd0b69
light <balathan2004@gmail.com>
2024-09-25 20:16:35 +0530

    no download

--- pages/api/test.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-import speak from "offline-tts";
-
-export default function (req, res) {
-  speak("hello world");
-  res.json({ message: "Hello, this is an offline text to speech example " });
-}
-
-// Example usage
